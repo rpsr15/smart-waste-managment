e8c0b7266780bb1dbd4683d553006352e300acda
Ravi <ravipratapsinghrathore15@gmail.com>
2018-09-06 12:54:09 +1000

    added website and applicatin plan document

---
+++ goClean/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var $:any;
+
+export interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+
+export const ROUTES: RouteInfo[] = [
+    { path: 'dashboard', title: 'Dashboard',  icon: 'ti-panel', class: '' },
+    { path: 'user', title: 'Profile',  icon:'ti-user', class: '' },
+    { path: 'table', title: 'Groups',  icon:'ti-view-list-alt', class: '' },
+    
+   
+    { path: 'maps', title: 'Maps',  icon:'ti-map', class: '' },
+    
+];
+
+@Component({
+    moduleId: module.id,
+    selector: 'sidebar-cmp',
+    templateUrl: 'sidebar.component.html',
+})
+
+export class SidebarComponent implements OnInit {
+    public menuItems: any[];
+    ngOnInit() {
+        this.menuItems = ROUTES.filter(menuItem => menuItem);
+    }
+    isNotMobileMenu(){
+        if($(window).width() > 991){
+            return false;
+        }
+        return true;
+    }
+
+}
---
+++ goClean/src/app/table/table.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+declare interface TableData {
+    headerRow: string[];
+    dataRows: string[][];
+}
+
+@Component({
+    selector: 'table-cmp',
+    moduleId: module.id,
+    templateUrl: 'table.component.html'
+})
+
+export class TableComponent implements OnInit{
+    public tableData1: TableData;
+    public tableData2: TableData;
+    ngOnInit(){
+        this.tableData1 = {
+            headerRow: [ 'ID', 'Name', 'Email', 'isNotified', 'Status'],
+            dataRows: [
+                ['1', 'Dakota Rice', 'rice@india.com', '✔', 'Active'],
+                ['2', 'Minerva Hooper', 'Curaçao@china.com', '✔', 'Active'],
+                ['3', 'Sage Rodriguez', 'chau@gmai.com', '✔', 'Deleted'],
+                ['4', 'Philip Chaney', 'tut@gmail.com', '✔', 'Active'],
+                ['5', 'Doris Greene', 'doris@outlook.com', '✘', 'Deleted'],
+                ['6', 'Mason Porter', 'porter@latrobe.edu.au', '✔', 'Deleted']
+            ]
+        };
+        this.tableData2 = {
+            headerRow: [ 'ID', 'Name',  'Salary', 'Country', 'City' ],
+            dataRows: [
+                ['1', 'Dakota Rice','$36,738', 'Niger', 'Oud-Turnhout' ],
+                ['2', 'Minerva Hooper', '$23,789', 'Curaçao', 'Sinaai-Waas'],
+                ['3', 'Sage Rodriguez', '$56,142', 'Netherlands', 'Baileux' ],
+                ['4', 'Philip Chaney', '$38,735', 'Korea, South', 'Overland Park' ],
+                ['5', 'Doris Greene', '$63,542', 'Malawi', 'Feldkirchen in Kärnten', ],
+                ['6', 'Mason Porter', '$78,615', 'Chile', 'Gloucester' ]
+            ]
+        };
+    }
+}
