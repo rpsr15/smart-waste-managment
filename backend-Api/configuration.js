284f87068c624204720cff98e0050b39697432d0
Jatin Mittal <jatinmittal2651@gmail.com>
2018-11-12 20:18:12 +1100

    added back end api , methods for the real time communications

---
+++ backend-Api/configuration.js
@@ -0,0 +1,11 @@
+exports.getPassword = function() {
+  return "mittal1994";
+};
+
+exports.getUser = function() {
+  return "jatinmittal2651@gmail.com";
+};
+
+exports.getService = function() {
+  return "Gmail";
+};
